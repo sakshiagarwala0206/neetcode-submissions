4ab87a0711fd1682f416f656dac921c4f56ad8e7
Sakshi Agarwala <95241724+sakshiagarwala0206@users.noreply.github.com>
2026-07-24 00:34:57 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0; i < nums.size(); i++)
+        {
+            int value = target - nums[i];
+            if(mp.find(value)!=mp.end())
+            {
+                return {mp[value],i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
